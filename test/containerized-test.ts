d6b6c33b3ea2136b54b4e12dd2dffb65fce327c2
Brandon Rajkowski <github@brajkowski.bulc.club>
2022-06-25 13:14:16 -0600

    containerized testing is now available as a shared testing method

---
+++ test/containerized-test.ts
@@ -0,0 +1,20 @@
+import { spawnSync } from "child_process";
+
+const hostCwd = process.cwd();
+const containerWd = "/app";
+
+function shareWithContainer(hostCwdFile: string): string {
+  return `-v ${hostCwd}/${hostCwdFile}:${containerWd}/${hostCwdFile}`;
+}
+
+export default function containerizedTest(
+  testCmd: string,
+  dockerImage: string,
+  sharedHostFiles: string[] = []
+): void {
+  const sharedFiles = sharedHostFiles.map(shareWithContainer).join(" ");
+  const dockerCmd = `docker run ${sharedFiles} --workdir ${containerWd} ${dockerImage} ${testCmd}`;
+  const result = spawnSync(dockerCmd, { stdio: "inherit", shell: true }).status;
+
+  result === null ? process.exit(-1) : process.exit(result);
+}

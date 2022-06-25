d13dc2a367c55cd60ac78ebd96dc61c15125bfc3
Brandon Rajkowski <github@brajkowski.bulc.club>
2022-06-25 12:27:42 -0600

    build is now bundled with esbuild (#8)

--- test/integration/test-util/install.ts
+++ test/integration/test-util/install.ts
@@ -1,7 +1,5 @@
 import { copyFileSync } from "fs";
 
 export function installHook(): void {
-  copyFileSync("dist/commit-msg-hook.js", ".git/hooks/commit-msg");
-  copyFileSync("dist/git-interop.js", ".git/hooks/git-interop.js");
-  copyFileSync("dist/config.js", ".git/hooks/config.js");
+  copyFileSync("dist/commit-msg", ".git/hooks/commit-msg");
 }

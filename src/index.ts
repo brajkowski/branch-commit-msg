d8a72aede003947d3f58c507ea67ee6e5c428fb0
Brandon Rajkowski <github@brajkowski.bulc.club>
2022-07-17 16:30:34 -0600

    updated yargs script name to match package name (#14)

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { hideBin } from "yargs/helpers";
 import { installAsGitHook } from "./install";
 
 yargs(hideBin(process.argv))
-  .scriptName("commit-msg")
+  .scriptName("branch-commit-msg")
   .command("install", "Install as a git commit-msg hook", installAsGitHook)
   .demandCommand()
   .showHelpOnFail(true)

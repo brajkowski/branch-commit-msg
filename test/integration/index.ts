95c8adbfe16a220b5f57e920120bd7de683a9612
Brandon Rajkowski <github@brajkowski.bulc.club>
2022-07-20 20:19:17 -0600

    integration and e2e test scripts allow args to be past from yarn scripts

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -1,15 +1,22 @@
 import containerizedTest from "../containerized-test";
 
-containerizedTest(
-  "yarn jest --testPathPattern=test/integration -i",
-  "node:16",
-  [
-    "node_modules",
-    "package.json",
-    "yarn.lock",
-    "jest.config.js",
-    "tsconfig.json",
-    "src",
-    "test",
-  ]
-);
+const yarnScriptArgs = process.argv.slice(2);
+const testCmd = [
+  "yarn",
+  "jest",
+  "--testPathPattern=test/integration",
+  "-i",
+  ...yarnScriptArgs,
+].join(" ");
+
+containerizedTest(testCmd, "node:16", [
+  "node_modules",
+  "package.json",
+  "yarn.lock",
+  "jest.config.shared.ts",
+  "jest.config.integration.ts",
+  "tsconfig.json",
+  "src",
+  "test",
+  "coverage",
+]);

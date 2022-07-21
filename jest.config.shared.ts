d11ab709ece064af8a596b9755d1dad6e5710400
Brandon Rajkowski <github@brajkowski.bulc.club>
2022-07-20 20:59:50 -0600

    build: added test coverage reporting (#20)
    * created jest configs for each testing level
    
    * integration and e2e test scripts allow args to be past from yarn scripts
    
    * test scripts now use different jest configurations
    
    * added test:coverage script that collects coverage
    
    * build workflow now produces code coverage reports
    
    * coverage reports are now uploaded to codecov
    
    * specified paths for collecting coverage

---
+++ jest.config.shared.ts
@@ -0,0 +1,14 @@
+import type { Config } from "@jest/types";
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  coverageReporters: ["clover"],
+  collectCoverageFrom: [
+    "src/**",
+    "!src/commit-msg-hook.ts",
+    "!src/index.ts",
+    "!src/install.ts",
+  ],
+};
+
+export default config;
--- test/e2e/index.ts
+++ test/e2e/index.ts
@@ -1,12 +1,23 @@
 import containerizedTest from "../containerized-test";
 
-containerizedTest("yarn jest --testPathPattern=test/e2e -i", "node:16", [
+const yarnScriptArgs = process.argv.slice(2);
+const testCmd = [
+  "yarn",
+  "jest",
+  "--testPathPattern=test/e2e",
+  "-i",
+  ...yarnScriptArgs,
+].join(" ");
+
+containerizedTest(testCmd, "node:16", [
   "node_modules",
   "package.json",
   "yarn.lock",
-  "jest.config.js",
+  "jest.config.shared.ts",
+  "jest.config.e2e.ts",
   "tsconfig.json",
   "src",
   "test",
   "dist",
+  "coverage",
 ]);

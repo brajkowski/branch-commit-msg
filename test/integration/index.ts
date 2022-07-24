36caaa0623f3e72ac6fe4317a1c2e641c0b0a98e
Brandon Rajkowski <github@brajkowski.bulc.club>
2022-07-24 14:51:47 -0600

    chore: relocated test configs files to ./test/config directory (#26)

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -4,7 +4,7 @@ const yarnScriptArgs = process.argv.slice(2);
 const testCmd = [
   "yarn",
   "jest",
-  "--testPathPattern=test/integration",
+  "--testPathPattern=integration",
   "-i",
   ...yarnScriptArgs,
 ].join(" ");
@@ -13,8 +13,6 @@ containerizedTest(testCmd, "node:16", [
   "node_modules",
   "package.json",
   "yarn.lock",
-  "jest.config.shared.ts",
-  "jest.config.integration.ts",
   "tsconfig.json",
   "src",
   "test",

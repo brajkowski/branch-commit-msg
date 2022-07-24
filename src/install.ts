d6dce4f8e9ffd588e5c28bd9a8e33157c5efe200
Brandon Rajkowski <github@brajkowski.bulc.club>
2022-07-24 17:37:54 -0600

    fix: install now throws helpful error when git is either not installed or the install is running outside of a git repo (#31)

--- src/install.ts
+++ src/install.ts
@@ -1,10 +1,21 @@
 import { copyFileSync } from "fs";
 import { join } from "path";
-import { repoRootDir } from "./git-interop";
+import { GitError, repoRootDir } from "./git-interop";
 
 export function installAsGitHook(): void {
   const commitMsgBinPath = join(__dirname, "commit-msg");
-  const root = repoRootDir();
+
+  let root: string;
+  try {
+    root = repoRootDir();
+  } catch (err) {
+    if (err instanceof GitError) {
+      throw Error(
+        "Could not find the root of the repository -- check that git is installed and that this install is running inside of a git repository."
+      );
+    }
+    throw err;
+  }
   const installPath = join(root, ".git", "hooks", "commit-msg");
   copyFileSync(commitMsgBinPath, installPath);
   console.log(`Installed hook to ${installPath}`);
---
+++ test/integration/install.spec.ts
@@ -0,0 +1,11 @@
+import { installAsGitHook } from "../../src/install";
+
+describe("install", () => {
+  describe("installAsGitHook()", () => {
+    it("should throw a helpful error if the install occurs outside of a git repo", () => {
+      expect(() => installAsGitHook()).toThrowError(
+        "Could not find the root of the repository -- check that git is installed and that this install is running inside of a git repository."
+      );
+    });
+  });
+});

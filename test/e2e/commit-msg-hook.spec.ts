e8f6058cffa2caababc0eea0a49471d9ed44e5c1
Brandon Rajkowski <github@brajkowski.bulc.club>
2022-07-30 19:57:47 -0600

    fix: amending commits no longer duplicates branch details

--- test/e2e/commit-msg-hook.spec.ts
+++ test/e2e/commit-msg-hook.spec.ts
@@ -114,4 +114,66 @@ describe("commit-msg-hook", () => {
       expect(latestCommitMessage()).toEqual(expectedMessage);
     }
   );
+
+  const messageAlreadyFormattedCases = [
+    {
+      branch: "branch",
+      originalMessage: "BRANCH: message",
+      expectedMessage: "branch: MESSAGE",
+      config: {
+        extractPattern: ".*",
+        extractPatternMatchCase: true,
+        commitMsgFormat: "%b0: %m | upper",
+      },
+    },
+    {
+      branch: "some/complex-123-5/branch",
+      originalMessage: "ADDED A FEATURE (ref: complex-123-5)",
+      expectedMessage: "added a feature (ref: complex-123-5)",
+      config: {
+        extractPattern: "complex[0-9-]+",
+        extractPatternMatchCase: true,
+        commitMsgFormat: "%m | lower (ref: %b0)",
+      },
+    },
+    {
+      branch: "some/CoMpLEX-123-5/branch",
+      originalMessage: "added a FEATURE (ref: complex-123-5)",
+      expectedMessage: "added a FEATURE (ref: CoMpLEX-123-5)",
+      config: {
+        extractPattern: "complex[0-9-]+",
+        extractPatternMatchCase: false,
+        commitMsgFormat: "%m (ref: %b0)",
+      },
+    },
+    {
+      branch: "some/CoMpLEX-123-5/branch",
+      originalMessage: "added a feature (REF: COMPLEX-123-5)",
+      expectedMessage: "ADDED A FEATURE (ref: complex-123-5)",
+      config: {
+        extractPattern: "complex[0-9-]+",
+        extractPatternMatchCase: false,
+        commitMsgFormat: "%m | upper (ref: %b0 | lower)",
+      },
+    },
+    {
+      branch: "some/CoMpLEX-123-5/BRANCH",
+      originalMessage: "SOME CoMpLEX-123-5 branch",
+      expectedMessage: "SOME CoMpLEX-123-5 branch",
+      config: {
+        extractPattern: "(some).*(complex[0-9-]+).*(branch)",
+        extractPatternMatchCase: false,
+        commitMsgFormat: "%b1 | upper %b2 %b3 | lower",
+      },
+    },
+  ];
+  test.each(messageAlreadyFormattedCases)(
+    "should prevent formatting an already formatted message (ie: git commit --amend)",
+    ({ branch, originalMessage, config, expectedMessage }) => {
+      createRcFile(config);
+      checkoutBranch(branch);
+      commit(originalMessage);
+      expect(latestCommitMessage()).toEqual(expectedMessage);
+    }
+  );
 });

daa2533aae14d53dbef03c79903dbc3a41d48b8a
Brandon Rajkowski <github@brajkowski.bulc.club>
2022-07-30 21:19:39 -0600

    fix: amending commits no longer duplicates branch details (#36)

--- src/commit-msg-hook.ts
+++ src/commit-msg-hook.ts
@@ -1,8 +1,56 @@
 #!/usr/bin/env node
+import escapeStringRegexp from "escape-string-regexp";
 import { readFileSync, writeFileSync } from "fs";
 import { exit } from "process";
 import { getConfig } from "./config";
 import { activeBranchName, GitError } from "./git-interop";
+
+enum RegExpFlag {
+  IGNORE_CASE = "i",
+}
+
+/**
+ * Prevent messages that are already in the proper format
+ * from being formatted again which would duplicate any matched
+ * branch details and non formatting token characters in the final commit message.
+ *
+ * This allows amending a commit message to work as expected.
+ *
+ * @param currentCommitMsg
+ * @param commitMsgFormat
+ * @param branchMatches
+ * @returns The current commit message removed of any formatting that may have already been applied.
+ */
+function dedupeCurrentCommitMsg(
+  currentCommitMsg: string,
+  commitMsgFormat: string,
+  branchMatches: RegExpMatchArray
+): string {
+  const nonTokenChars = commitMsgFormat
+    // Find all formatting tokens.
+    .match(/(%m|%b[0-9]+)(\s\|\s(lower|upper))?/g)
+
+    // Remove all formatting tokens -- what remains are the non formatting token characters.
+    ?.reduce(
+      (msgFormat, formatToken) => msgFormat.replace(formatToken, ""),
+      commitMsgFormat
+    );
+
+  let currentCommitMsgDeduped = branchMatches.reduce(
+    // Remove any matched branch details.
+    (msg, branchDetail) =>
+      msg.replace(new RegExp(branchDetail, RegExpFlag.IGNORE_CASE), ""),
+    currentCommitMsg
+  );
+  if (nonTokenChars) {
+    // Remove any non formatting token characters.
+    currentCommitMsgDeduped = currentCommitMsgDeduped.replace(
+      new RegExp(escapeStringRegexp(nonTokenChars), RegExpFlag.IGNORE_CASE),
+      ""
+    );
+  }
+  return currentCommitMsgDeduped;
+}
 
 let branchName: string;
 const commitMsgFilePath = process.argv[process.argv.length - 1];
@@ -25,7 +73,7 @@ try {
 const branchMatches = branchName.match(
   new RegExp(
     hookConfig.extractPattern,
-    hookConfig.extractPatternMatchCase ? undefined : "i"
+    hookConfig.extractPatternMatchCase ? undefined : RegExpFlag.IGNORE_CASE
   )
 );
 
@@ -33,16 +81,23 @@ if (!branchMatches) {
   exit(0);
 }
 
-let newCommitMsg = hookConfig.commitMsgFormat;
-branchMatches.forEach((b, index) => {
-  newCommitMsg = newCommitMsg
-    .replace(`%b${index} | upper`, b.toUpperCase())
-    .replace(`%b${index} | lower`, b.toLowerCase())
-    .replace(`%b${index}`, b);
-});
-newCommitMsg = newCommitMsg
-  .replace("%m | upper", currentCommitMsg.toUpperCase())
-  .replace("%m | lower", currentCommitMsg.toLowerCase())
-  .replace("%m", currentCommitMsg);
+const currentCommitMsgDeduped = dedupeCurrentCommitMsg(
+  currentCommitMsg,
+  hookConfig.commitMsgFormat,
+  branchMatches
+);
+
+const newCommitMsg = branchMatches
+  .reduce(
+    (msg, branchDetail, index) =>
+      msg
+        .replace(`%b${index} | upper`, branchDetail.toUpperCase())
+        .replace(`%b${index} | lower`, branchDetail.toLowerCase())
+        .replace(`%b${index}`, branchDetail),
+    hookConfig.commitMsgFormat
+  )
+  .replace("%m | upper", currentCommitMsgDeduped.toUpperCase())
+  .replace("%m | lower", currentCommitMsgDeduped.toLowerCase())
+  .replace("%m", currentCommitMsgDeduped);
 
 writeFileSync(commitMsgFilePath, newCommitMsg);
